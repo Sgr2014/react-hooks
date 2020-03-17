86cc0d400aa11199a5e0c9b65da4aca1e416883d
huxubin <xbhu2014@126.com>
2020-03-17 13:52:08 +0800

    <fix>(<scope>): <组件逻辑修改> <BLANK LINE> <fix ClassComponent component function> <BLANK LINE> <none bug>

--- src/pages/ClassComponent.js
+++ src/pages/ClassComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class ClassComponent extends Component {
   render () {
-    const name = 'react'
+    const name = 'react dom'
     const user = {'firstName': 'John', 'lastName': 'Bull'}
     function formatterName () {
       return user.firstName + ' ' + user.lastName
